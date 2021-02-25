d5ecf8589696eeee2b4f468d65b1c901f8d48767
Mizuki Morohoshi <8080suisho@gmail.com>
2021-02-25 17:51:30 +0900

    small update

---
+++ Assets/GameOverScripts/GameOverManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public AudioClip sound;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = sound;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
