4065003d648a9d6ed6a24d442d6a81d01202778b
Mizuki Morohoshi <8080suisho@gmail.com>
2021-03-03 17:28:04 +0900

    クリア画面

---
+++ Assets/GameClearScripts/GameClearManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClearManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RePlay()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ToStageSelect()
+    {
+
+    }
+}
